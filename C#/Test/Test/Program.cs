a4a16e65a6a29901a2fcc0ee27789301c08496a6
Khaled Hammoun <kh.hammoun@gmail.com>
2020-12-20 00:39:21 +0100

    initail commit

---
+++ C#/Test/Test/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] trying = new int[2][];
+
+            trying[1][1] = 123;
+            Console.WriteLine(trying[1][1]);
+        }
+    }
+}
